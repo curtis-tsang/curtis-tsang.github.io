ffce57d0e753dd3c1b7bd637557c07adb2348a76
curtis-tsang <135094873+curtis-tsang@users.noreply.github.com>
2023-12-22 23:17:58 +0800

    Add files via upload

---
+++ own_web_2/app.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entries) => {
+        if(entries.isIntersecting){
+            entries.target.classList.add("appear");
+        }
+        else{
+            entries.target.classList.remove("appear");
+        }
+    });
+})
+
+const hidden_item = document.querySelectorAll(".hidden");
+hidden_item.forEach((el) => observer.observe(el));
